14e200acacfafc890e0a658c136e1be4fa074c50
meow10811 <10765599+meow10811@users.noreply.github.com>
2020-11-22 16:55:48 -0500

    Added methods for test cases

---
+++ src/test/java/assignment3/DogShelterTest.java
@@ -0,0 +1,44 @@
+package assignment3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DogShelterTest {
+
+    @Test
+    void shelter() {
+    }
+
+    @Test
+    void adopt() {
+    }
+
+    @Test
+    void testAdopt() {
+    }
+
+    @Test
+    void findOldest() {
+    }
+
+    @Test
+    void findYoungest() {
+    }
+
+    @Test
+    void findDogToAdopt() {
+    }
+
+    @Test
+    void budgetVetExpenses() {
+    }
+
+    @Test
+    void getVetSchedule() {
+    }
+
+    @Test
+    void iterator() {
+    }
+}
